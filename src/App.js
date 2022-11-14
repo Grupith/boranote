98773cb2e685cb1c5b6d516a4995cfd8ac253cca
Dylan Koss <dylanhkoss@gmail.com>
2022-11-14 12:08:08 -0600

    styled notes container and fixed button spacing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Note from './components/Note';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className='noteContainer'>
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+      </div>
+        
     </div>
   );
 }
---
+++ src/components/Note.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Note() {
+  return (
+    <div className='note'>
+        <h1>Title</h1>
+        <p> 
+            This is just some
+            example text of 
+            what a note would
+            look like in the UI.
+            I think it is a great
+            lookin UI for this.This is just some
+            example text of 
+            what a note would
+            look like in the UI.This is just some
+            example text of 
+            what a note would
+            look like in the UI.
+            I think it is a great
+            lookin UI for this.This is just some
+            example text of 
+            what a note would
+            look like in the UI.
+            
+        </p>
+        <div className='noteButtonWrapper'>
+            <button>Edit</button>
+            <button>Delete</button>
+        </div>
+        
+    </div>
+  )
+}
