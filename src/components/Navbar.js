1c5984072e7d391fff87ee040cac2a00ec5bb0d6
Dylan Koss <dylanhkoss@gmail.com>
2022-11-14 01:22:56 -0600

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { GiHamburgerMenu  } from "react-icons/gi";
 
 export default function Navbar() {
   return (
     <nav className='navbar'>
         <h1 className='title'>Boranote</h1>
-        <button>Create note</button>
+        <div className='navWrapper'>
+            <button>Create note</button>
+            <GiHamburgerMenu size={36} className='hamburgerMenu'/>
+        </div>
+        
     </nav>
   )
 }
